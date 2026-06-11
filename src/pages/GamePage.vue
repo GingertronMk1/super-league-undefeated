0efe65e062fb6d5f8f8c994b370748c6a7ad3361
Jack Ellis <jackellis1504@gmail.com>
2026-06-11 17:00:38 +0100

    added game page, here we go
    AM src/pages/GamePage.vue

---
+++ src/pages/GamePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { usePlayersStore } from '@/stores/players.ts'
+import { computed, ref } from 'vue'
+
+const playersStore = usePlayersStore()
+const players = computed(() => playersStore.players)
+const chosenTeam = ref(null)
+const draftedTeam = ref({})
+</script>
+
+<template>
+</template>
+
+<style scoped></style>
